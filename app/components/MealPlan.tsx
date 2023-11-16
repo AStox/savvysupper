f94b3783dd854e877ee69ca1fcdcc55792e37406
Adam Stox <adamstox@protonmail.com>
2023-11-16 18:30:37 -0500

    fix missing prop

--- app/components/MealPlan.tsx
+++ app/components/MealPlan.tsx
@@ -16,7 +16,7 @@ const MealPlan: React.FC<MealPlanProps> = ({ meals, totalCost, totalSavings }) =
     <div className="container mx-auto px-4 py-32 md:px-8 lg:px-16">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
         {meals.map((meal) => (
-          <MealCard key={meal.id} meal={meal} />
+          <MealCard key={meal.id} meal={meal} onReroll={() => {}} />
         ))}
         <MealCustomizationCard key="customization" />
         <WeeklySummaryCard key="summary" totalCost={totalCost} totalSavings={totalSavings} />
