6a72710388f6a8d25a39e48b874e56ca618df679
Adam Stox <adamstox@protonmail.com>
2023-12-17 22:40:11 -0600

    Update ingredient scraper

--- pages/api/ingredientScraper.ts
+++ pages/api/ingredientScraper.ts
@@ -10,17 +10,58 @@ interface Ingredient {
   onSale: boolean;
 }
 
-const meatAndSeafood =
-  "https://voila.ca/products?sublocationId=bfe1b4c5-2ada-435c-ab79-e02ddf744d1d";
-const fruitsAndVegetables =
-  "https://voila.ca/products?sublocationId=43a936d1-df1d-4bf1-a09c-b23c6a8edf63";
-const dairyAndEggs = "https://voila.ca/products?sublocationId=4cbdd52d-e935-4f09-a684-8dc22c14bc98";
-const cheese = "https://voila.ca/products?sublocationId=48a29c49-0a08-48c2-98bf-83e63f927d19";
-const breadAndBakery =
-  "https://voila.ca/products?sublocationId=e6ec3585-d317-4729-ab3a-eec5ba88f231";
-const plantBased = "https://voila.ca/products?sublocationId=6ef9b3da-5cd8-495e-8dd2-b3f1764ad313";
-const international =
-  "https://voila.ca/products?sublocationId=a14f9339-0c02-45cd-81a0-1e02c43a0188";
+const pages = {
+  "Fresh Fruits & Vegetables": [
+    "Fresh Fruits",
+    "Fresh Vegetables",
+    "Fresh Cut Fruits & Vegetables",
+    "Fresh Herbs",
+    "Fresh Organic Fruits",
+    "Fresh Organic Vegetables",
+  ],
+  "Meat & Seafood": [
+    "Beef & Veal",
+    "Chicken",
+    "Pork",
+    "Turkey",
+    "Lamb",
+    "Fish",
+    "Exotic Meats",
+    "Fish & Seafood",
+    "Bacon",
+    "Hot Dogs & Sausages",
+  ],
+  "Dairy & Eggs": [
+    "Milk",
+    "Cream & Creamers",
+    "Butter & Margarine",
+    "Yogurt",
+    "Sour Cream",
+    "Whipped Cream",
+    "Eggs",
+  ],
+  Cheese: [
+    "Artisan Cheese",
+    "Shredded & Grated Cheese",
+    "Block Cheese",
+    "Cottage Cheese",
+    "Soft & Spreadable Cheese",
+    "Cream Cheese",
+  ],
+};
+
+async function findLink(page: Page, linkText: string): Promise<string | null> {
+  // Use XPath to find the link based on its text content
+  const linkXPath = `//a[contains(text(), "${linkText}")]`;
+  const links = await page.$x(linkXPath);
+
+  if (links.length > 0) {
+    const href = await page.evaluate((el) => el.getAttribute("href"), links[0]);
+    return href ? `https://voila.ca${href}` : null;
+  }
+
+  return null;
+}
 
 async function scrapeUrl(page: Page, url: string): Promise<Ingredient[]> {
   await page.goto(url, { waitUntil: "networkidle2" });
@@ -45,13 +86,15 @@ async function scrapeUrl(page: Page, url: string): Promise<Ingredient[]> {
             "0"
         );
         const onSale = !!el.querySelector('span[data-test="fop-offer-text"]');
+        const priceElements = el.querySelectorAll('div[data-test="fop-price-wrapper"] span');
+        const prices = Array.from(priceElements)
+          .map((span) => span.textContent?.trim())
+          .filter((text) => text && text.startsWith("$"));
+
         const regularPrice = onSale
-          ? parseFloat(
-              el
-                .querySelector('div[data-test="fop-price-wrapper"] span:nth-child(2)')
-                ?.textContent?.replace("$", "")
-                .trim() || "0"
-            )
+          ? prices.length > 1
+            ? parseFloat(prices[1].replace("$", ""))
+            : parseFloat(prices[0].replace("$", ""))
           : currentPrice;
 
         return { title, quantity, currentPrice, onSale, regularPrice };
@@ -59,7 +102,7 @@ async function scrapeUrl(page: Page, url: string): Promise<Ingredient[]> {
     );
 
     newItems.forEach((item) => {
-      if (item.title && item.onSale && item.currentPrice > 0) {
+      if (item.title && item.currentPrice > 0) {
         if (!addedTitles.has(item.title)) {
           items.push(item as Ingredient);
           addedTitles.add(item.title);
@@ -70,6 +113,7 @@ async function scrapeUrl(page: Page, url: string): Promise<Ingredient[]> {
 
     await page.evaluate(() => window.scrollBy(0, window.innerHeight));
     await page.waitForTimeout(1000);
+    console.log("scrolling...");
 
     reachedBottom = await page.evaluate(() => {
       return window.scrollY + window.innerHeight >= document.documentElement.scrollHeight;
@@ -87,85 +131,51 @@ async function scrapeUrl(page: Page, url: string): Promise<Ingredient[]> {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Launch Puppeteer in non-headless mode
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
-
-  const urls = [
-    // { name: "meatAndSeafood", url: meatAndSeafood },
-    // { name: "fruitsAndVegetables", url: fruitsAndVegetables },
-    // { name: "dairyAndEggs", url: dairyAndEggs },
-    // { name: "cheese", url: cheese },
-    // { name: "breadAndBakery", url: breadAndBakery },
-    // { name: "plantBased", url: plantBased },
-    // { name: "international", url: international },
-    {
-      name: "chickenBreast",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=ed0ab052-00b0-49ed-815a-b8faec03e332",
-    },
-    {
-      name: "wholeChicken",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=f716a0b3-9a60-40f4-96f5-e46f57a48b59",
-    },
-    {
-      name: "chickenThighsDrumsticksWings",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=7057f1c2-840d-4f76-aeb8-52f83d659315",
-    },
-    {
-      name: "groundChickenAndBurgers",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=a23bf7ee-a337-4d10-8f72-47ad57970ba2",
-    },
-    {
-      name: "chickenBacon",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=319636cb-007b-4e4d-91b3-4eacbcfed975",
-    },
-    {
-      name: "beefAndVeal",
-      url: "https://voila.ca/products?source=navigation&sublocationId=dce34da9-8018-4916-9c5d-7fc6427e354a",
-    },
-    {
-      name: "turkey",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=d1dc09ce-d009-4d95-811a-bbe31c9fba30",
-    },
-    {
-      name: "pork",
-      url: "https://voila.ca/products?sublocationId=5637ec5b-0aa6-4972-8585-6a15d78b9799",
-    },
-    {
-      name: "lamb",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=207a429c-0ff9-4d22-87d4-451dbf93fc0f",
-    },
-    {
-      name: "freshFish",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=0c370bc0-95e5-44f0-8894-8e08e798028a",
-    },
-    {
-      name: "smokedAndPreparedFish",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=8e23bfb0-14e2-437e-8bcf-e4d0682f1056",
-    },
-    {
-      name: "freshSeafood",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=093c1ea7-07fd-4650-be73-3ebc12506a1d",
-    },
-    {
-      name: "frozenSeafood",
-      url: "https://voila.ca/products?sortBy=favorite&sublocationId=41fdb84a-1bdf-4402-acaf-385a27f6de1d",
-    },
-    {
-      name: "frozenFish",
-      url: "https://voila.ca/products?brands=Compliments&sortBy=favorite&sublocationId=bb2090c8-cfda-4cf7-96ad-3f692cd739a3",
-    },
-  ];
-
-  let allItems: Ingredient[] = [];
+  await page.setViewport({ width: 1920, height: 1080 });
 
   try {
-    for (let i = 0; i < urls.length; i++) {
-      console.log(`Scraping ${urls[i].name}... (page ${i + 1} of ${urls.length})`);
-      const items = await scrapeUrl(page, urls[i].url);
-      allItems = allItems.concat(items);
-      console.log(`Total scraped items so far: ${allItems.length}`);
+    // Calculate the total number of subcategories to be scraped
+    const totalSubcategories = Object.values(pages).flat().length;
+    let currentSubcategoryNumber = 0;
+
+    for (const category in pages) {
+      // Navigate to the main navigation page before each category
+      await page.goto("https://voila.ca/products?source=navigation", { waitUntil: "networkidle2" });
+
+      const categoryLink = await findLink(page, category);
+      if (!categoryLink) throw new Error(`${category} link not found`);
+      await page.goto(categoryLink, { waitUntil: "networkidle2" });
+
+      let allCategoryItems: Ingredient[] = [];
+
+      for (const subcategory of pages[category]) {
+        currentSubcategoryNumber++;
+        console.log(
+          `Scraping page ${currentSubcategoryNumber}/${totalSubcategories}: ${subcategory} in ${category}`
+        );
+
+        const subcategoryLink = await findLink(page, subcategory);
+        if (!subcategoryLink) throw new Error(`${subcategory} link not found`);
+        await page.goto(subcategoryLink, { waitUntil: "networkidle2" });
+
+        const items = await scrapeUrl(page, subcategoryLink);
+        allCategoryItems = allCategoryItems.concat(items);
+
+        // Navigate back to the main category page before moving to the next subcategory
+        await page.goto(categoryLink, { waitUntil: "networkidle2" });
+      }
+
+      // Write results to a file for each category
+      fs.writeFileSync(
+        `${category.replace(/\s+/g, "_")}_ingredients.json`,
+        JSON.stringify(allCategoryItems, null, 2)
+      );
     }
+
+    console.log("Scraping completed for all categories");
+    res.status(200).json({ message: "Scraping completed for all categories" });
   } catch (error) {
     console.error("Error during scraping:", error);
     res.status(500).json({ error: "Error during scraping" });
@@ -173,12 +183,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   } finally {
     await browser.close();
   }
-
-  console.log(`Total items scraped: ${allItems.length}`);
-  // Write results to a file
-  fs.writeFileSync("sobeys_protein_ingredients.json", JSON.stringify(allItems, null, 2));
-
-  res
-    .status(200)
-    .json({ message: "Scraping completed and data written to sobeys_ingredients.json" });
 }
