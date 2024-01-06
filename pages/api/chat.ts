9f3d7cb055f18fb1524c58d2ec8715f6b67dfeee
Adam Stox <adamstox@protonmail.com>
2024-01-05 22:19:15 -0600

    Add maxDuration to chat

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import OpenAI from "openai";
+export const maxDuration = 300;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
